153b601cd51923a3f59b47e3784ef33be9164d0a
lktp <bassmaster12@gmail.com>
2026-06-08 20:28:21 +0000

    Add files via upload

--- sys_control/views.py
+++ sys_control/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from characterization.models import ArpEntry
+from characterization.models import ArpEntry, Network
 from hosts.models import Host
 from topology.models import PortAttachment
 from .utils import reconcile_network_topology # Your reconciliation function
@@ -14,6 +14,7 @@ def sys_control_view(request):
             PortAttachment.objects.all().delete()
             ArpEntry.objects.all().delete()
             Host.objects.all().delete()
+            Network.objects.all().delete()
             messages.success(request, "All network data has been cleared.")
             
         elif action == 'run_reconciliation':
